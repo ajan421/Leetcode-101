30ef1dcd5f290d289438067cb94bbd61a6c7cd8d
Ajan <117378964+ajan421@users.noreply.github.com>
2024-03-18 23:36:36 +0530

    Time: 10 ms (44.84%), Space: 8.5 MB (79.81%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int res=s.compare(t);
+        if(res==0)
+            return 1;
+        else 
+            return 0;
+    }
+};
